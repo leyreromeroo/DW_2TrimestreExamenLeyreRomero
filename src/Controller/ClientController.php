fe03545adf95c8c5384e89f2b9ca4d5fb649b286
Leyre Romero Velasco <leyre.romero@gmail.com>
2026-01-30 12:13:05 +0100

    Corregir importación de Service\ClientMapper en ClientController

--- src/Controller/ClientController.php
+++ src/Controller/ClientController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Client;
 use App\Repository\ClientRepository;
+use App\Service\ClientMapper;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
